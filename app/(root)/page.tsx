fd566f0b2fbdc5b6ac417f4526c9f101ed55f7f0
Berentolkien <bernalb2014@gmail.com>
2024-06-28 10:57:43 -0300

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import { HeaderBox } from '@/components/HeaderBox';
+import  RightSideBar  from '@/components/RightSideBar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home = () => {
+  const loggedIn = { firstName: 'Lucas', lastName: "Bernaola", email: 'bernadev7@gmail.com' };
+
   return (
-    <div>page</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox 
+            type="greeting"
+            title='Welcome'
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+
+
+        recent transactions
+      </div>
+
+      <RightSideBar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 125.50}, {currentBalance: 125.50}]}
+      />
+    </section>
   )
 }
 
---
+++ app/(root)/transaction-history/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const TransactionsHistory = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+
+export default TransactionsHistory;
